eac0b601502d42525011f67aecca43bc4e81ca8e
klnavaneet <klnavaneet@gmail.com>
2019-06-03 09:51:12 +0530

    made data_loader more modular to ease loading data for varied experiments

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,7 @@
 import os
 from os.path import join
 import sys
+import pdb
 
 import numpy as np
 from itertools import product
@@ -23,16 +24,16 @@ def read_image(img_path, normalize=True):
     returns:
         img_rgb: float; output image, in rgb format if color image
     '''
-    img = cv2.imread(rgb_path)
+    img = cv2.imread(img_path)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_rgb = np.astype(img_rgb)
+    img_rgb = img_rgb.astype(np.float32)
     if normalize:
         img_rgb = img_rgb / 255.
 
     return img_rgb
 
 
-def read_angles(angles_path, to_rad=True):
+def read_angles(angles_path, angles=None, i=0, to_rad=True):
     '''
     Read values of pose angles from file, convert to radians.
 
@@ -43,20 +44,22 @@ def read_angles(angles_path, to_rad=True):
     returns:
         angle_x, angle_y: float, (); rotation angles wrt x and y axes
     '''
-    with open(angles_path, 'r') as fp:
-        angles = [item.split('\n')[0] for item in fp.readlines()]
-    angle = angles[i % 10]
-    angle_x = float(angle.split(' ')[0])
-    angle_y = float(angle.split(' ')[1])
-    # Convert from degrees to radians
-    if to_rad:
-        angle_x = angle_x * np.pi/180.
-        angle_y = angle_y * np.pi/180.
+    if angles == None:
+        with open(angles_path, 'r') as fp:
+            angles = [item.split('\n')[0] for item in fp.readlines()]
+        return angles
+    else:
+        angle = angles[i % 10]
+        angle_x = float(angle.split(' ')[0])
+        angle_y = float(angle.split(' ')[1])
+        # Convert from degrees to radians
+        if to_rad:
+            angle_x = angle_x * np.pi/180.
+            angle_y = angle_y * np.pi/180.
+        return angle_x, angle_y
 
-    return angle_x, angle_y
 
-
-def fetch_batch_drc(models, indices, batch_num, batch_size, args=None):
+def fetch_batch_drc(models, indices, batch_num, args=None):
     '''
     Obtain batch data for training.
 
@@ -89,8 +92,8 @@ def fetch_batch_drc(models, indices, batch_num, batch_size, args=None):
         batch_partseg = []
         batch_data.append(batch_partseg)
 
-    for ind in indices[(batch_num*batch_size):
-                       (batch_num*batch_size + batch_size)]:
+    for ind in indices[(batch_num*args.batch_size):
+                       (batch_num*args.batch_size + args.batch_size)]:
         model_gt, model_x, model_y = [[] for i in range(3)]
         model_data = [model_gt, model_x, model_y]
         if args.rgb:
@@ -107,19 +110,19 @@ def fetch_batch_drc(models, indices, batch_num, batch_size, args=None):
         ip_image = cv2.resize(ip_image, (args.IMG_W, args.IMG_H))
         batch_ip.append(ip_image)
         batch_names.append(model_name)
-
+        angles_path = join(model_path, 'view.txt')
+        angles = read_angles(angles_path)
 
         for i in range(0, args.N_VIEWS):
             if args.CORR:
                 # To obtain outputs corresponding to input view
                 i = ind[1]
-            proj_path = join(model_path, 'depth_%d.png' % (i % 10))
+            proj_path = join(model_path, 'mask_%d.png' % (i % 10))
             rgb_path = join(model_path, 'render_%d.png' % (i % 10))
             view_path = join(model_path, 'camera_%d.mat' % (i % 10))
-            angles_path = join(model_path, 'view.txt')
             ip_proj = read_image(proj_path, normalize=False)[:, :, 0]
             # Read rotation angles in radians from file
-            angle_x, angle_y = read_angles(angles_path)
+            angle_x, angle_y = read_angles(angles_path, angles, i)
             model_vals = [ip_proj, angle_x, angle_y]
             if args.rgb:
                 # Read inputs and normalize to [0,1] range
@@ -138,17 +141,17 @@ def fetch_batch_drc(models, indices, batch_num, batch_size, args=None):
         # Append the instance-wise data into the batch list
         _ = [batch_item.append(model_item) for batch_item, model_item in
              zip(batch_data, model_data)]
-    batch_out = [batch_ip, batch_rgb, batch_partseg, batch_gt,
-                 batch_x, batch_y]
+    batch_out = [batch_ip]
+    batch_out.extend(batch_data)
     batch_out = [np.array(item) for item in batch_out]
-    batch_out.append(batch_names)
+    batch_out.extend([batch_names])
 
     return batch_out
 
 
-def fetch_labels(model_path):
+def fetch_labels_partseg(model_path):
     '''
-    Obtain part segmentation class labels for point cloud.
+    Obtain GT part segmentation class labels for point cloud.
 
     args:
         model_path: str; directory of the pcl model
@@ -179,61 +182,47 @@ def fetch_labels_rgb(model_path):
     return colors_gt
 
 
-def fetch_batch_pcl_rgb(models, indices, batch_num, batch_size):
-    batch_ip = []
+def fetch_batch_pcl(models, indices, batch_num, args):
     batch_gt = []
-    batch_rgb = []
-    for ind in indices[(batch_num*batch_size):
-                       (batch_num*batch_size + batch_size)]:
+    if args.rgb:
+        batch_rgb = []
+    if args.partseg:
+        batch_partseg = []
+
+    for ind in indices[(batch_num*args.batch_size):
+                       (batch_num*args.batch_size + args.batch_size)]:
         model_path = models[ind[0]]
         pcl_path = join(model_path, 'pcl_1024_fps_trimesh.npy')
-        pcl_gt = np.load(pcl_path)
-        pcl_rgb = fetch_labels_rgb(model_path)
-        rotate = False
-        if rotate:
-            metadata_path = join(
-                model_path, 'rendering', 'rendering_metadata.txt'
-                )
-            with open(metadata_path, 'r') as f:
-                metadata = f.readlines()
-                metadata = [i.strip() for i in metadata]
-                x = [float(i.split(' ')[0]) for i in metadata]
-                y = [float(i.split(' ')[1]) for i in metadata]
-                xangle = np.pi/180. * x[ind[1]]
-                yangle = np.pi/180. * y[ind[1]]
-                pcl_gt = rotate(pcl_gt, xangle=xangle, yangle=yangle)
         try:
+            pcl_gt = np.load(pcl_path)
             batch_gt.append(pcl_gt)
-            batch_rgb.append(pcl_rgb)
+            if args.rgb:
+                pcl_rgb = fetch_labels_rgb(model_path)
+                batch_rgb.append(pcl_rgb)
+            if args.partseg:
+                labels_gt = fetch_labels_partseg(model_path)
+                batch_partseg.append(labels_gt)
         except:
             pass
+    batch_data = []
     batch_gt = np.array(batch_gt)
-    batch_rgb = np.array(batch_rgb)
+    batch_data.append(batch_gt)
+    if args.rgb:
+        batch_rgb = np.array(batch_rgb)
+        batch_data.append(batch_rgb)
+    if args.partseg:
+        batch_partseg = np.array(batch_partseg)
+        batch_data.append(batch_partseg)
 
-    return batch_ip, batch_gt, batch_rgb
-
-
-def fetch_batch_seg(dataset, models, indices, batch_num, batch_size):
-    batch_ip = []
-    batch_gt = []
-    batch_label_wts = []
-    for ind in indices[(batch_num*batch_size):
-                       (batch_num*batch_size + batch_size)]:
-        model_path = models[ind]
-        pcl_inp = fetch_pcl(dataset, model_path)
-        labels_gt = fetch_labels(model_path)
-        batch_ip.append(pcl_inp)
-        batch_gt.append(labels_gt)
-    batch_ip = np.array(batch_ip)
-    batch_gt = np.array(batch_gt)
-    return batch_ip, batch_gt
+    return batch_data
 
 
 def get_shapenet_drc_models(data_dir, categs=['03001627'],
                             num_views=10):
     '''
     Obtain indices and names of all point cloud models, for train and
-    validation sets
+    validation sets.
+
     args:
         data_dir: str; root directory containing data of all
                         categories
@@ -279,10 +268,8 @@ def get_shapenet_drc_models(data_dir, categs=['03001627'],
         len(val_models), len(val_models)*num_views
         )
 
-    return (
-            train_models, val_models,
-            train_pair_indices, val_pair_indices
-            )
+    return (train_models, val_models,
+            train_pair_indices, val_pair_indices)
 
 
 def get_drc_models_util(data_dir, category, eval_set):
@@ -292,14 +279,16 @@ def get_drc_models_util(data_dir, category, eval_set):
     else:
         cats = [category]
     for cat in cats:
-        category_id = shapenet_category_to_id[cat]
+        categ_id = shapenet_category_to_id[cat]
         splits_file_path = join(
-            data_dir, 'splits', category_id +
+            data_dir, 'splits', categ_id +
             '_%s_list.txt' % eval_set)
+        models_list = np.load(splits_file_path)
+        models.extend([join(data_dir, categ_id, item) for item in models_list])
         with open(splits_file_path, 'r') as f:
             for model in f.readlines():
                 models.append(
-                    join(data_dir, category_id, model.strip()))
+                    join(data_dir, categ_id, model.strip()))
 
     return models
 
@@ -326,7 +315,8 @@ def get_drc_models_seg(dataset, data_dir, category, eval_set):
 
 def get_feed_dict(models, indices, models_pcl, b, args):
     '''
-    Obtain batch data for training
+    Obtain batch data for training.
+
     args:
         models: list of all ids/names of input image models
         indices: indices to be chosen from models for the current
@@ -336,6 +326,10 @@ def get_feed_dict(models, indices, models_pcl, b, args):
         args: input arguments while running the train.py file
     returns:
         All outputs are lists of length N_VIEWS.
+        batch_img always contains [batch_ip, batch_mask, batch_x, batch_y] 
+        and conditionally contains [batch_rgb, batch_partseg]
+        batch_pcl always contains [batch_pcl_gt] and conditionally 
+        contains [batch_labels_rgb, batch_labels_partseg]
         Properties of each element in the list:
         batch_ip: uint8, (BS,IMG_H,IMG_W,3); input rgb images
         batch_gt: float, (BS,IMG_H,IMG_W); GT foreground masks
@@ -348,24 +342,21 @@ def get_feed_dict(models, indices, models_pcl, b, args):
         batch_y: float, (); rotation angle along y-axis for the
                                 view point in radians
     '''
-    batch = fetch_batch_drc(models, indices, b, args.batch_size, args)
-    (batch_ip, batch_rgb, _, batch_gt_mask,
-        batch_x, batch_y, model_names) = batch
+    batch_img = fetch_batch_drc(models, indices, b, args)
 
     # pcl gt
-    _, batch_pcl_gt, _ = fetch_batch_pcl_rgb(
-        models_pcl, indices, b, args.batch_size
-        )
+    batch_pcl = fetch_batch_pcl(models_pcl, indices, b, args)
 
     # Align axes according to the co-ordinate system of renderer
-    batch_pcl_gt = preprocess_pcl_gt(batch_pcl_gt)
+    batch_pcl[0] = preprocess_pcl_gt(batch_pcl[0])
 
-    return (batch_ip, batch_rgb, batch_gt_mask, batch_pcl_gt,
-            batch_x, batch_y, model_names)
+    return batch_img, batch_pcl
 
 
 def preprocess_pcl_gt(pcl):
     '''
+    Align GT pcl in canonical orientation.
+
     To align the GT pcl according to the axes of the GT image renderer
     (i.e.  the co-ordinate system used while rendering the images from
     GT PCL), interchange the axes and change axes directions
